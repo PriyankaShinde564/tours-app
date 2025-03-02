283728947b3ce5d7a8c3da9d57043e4d781e0e7b
priyanka shinde <shindepriyanka564@gmail.comm>
2025-03-02 19:52:26 +0530

    updated image for home page

--- src/component/Packages/Packages.tsx
+++ src/component/Packages/Packages.tsx
@@ -19,8 +19,8 @@ export default function Packages() {
   //   "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
   // const image2 =
   //   "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg";
-  const image3 =
-    "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
+  const image3 ="https://media.istockphoto.com/id/2157393625/photo/collage-of-india-monuments.jpg?s=1024x1024&w=is&k=20&c=oJXbyqF9j-eseFCRXoGwLJlYQ8apXYl9Rdi6nVbjzeA="
+    //"https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
   // const image4 =
   //   "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg";
   // const image5= "../../assets/images.jpg";
